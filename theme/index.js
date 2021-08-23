22c8fdef87833d5af401360dad05b699ff496376
Andrés Proaño Valencia <andres@oventi.org>
2021-08-23 23:57:16 +1200

    refactored request, then added build function

--- theme/index.js
+++ theme/index.js
@@ -4,17 +4,25 @@ export async function request(path, data) {
   const {entries, ...global} = data
   const is_home = path === '/'
   const template = is_home ? 'home' : 'entry'
-
-  const page = is_home
+  const page_data = is_home
     ? {title: 'danta', entries}
-    : entries.find(p => path === `/${p.slug}`)
+    : entries.find(e => path === `/${e.slug}`)
 
-  if(!page) {
+  if(!page_data) {
     return {content: `${path} not found`, status: 404}
   }
 
-  return {content: danta.render(template, {global, ...page})}
+  return {content: danta.render(template, {global, ...page_data})}
 }
 
-export async function build(project_name, templates, base_data) {
+export async function build(data) {
+  const {entries, ...global} = data
+
+  // build home
+  await danta.build('index.html', 'home', {global, title: 'danta', entries})
+
+  // build entry pages
+  for(const entry of entries) {
+    await danta.build(`${entry.slug}.html`, 'entry', {global, ...entry})
+  }
 }
